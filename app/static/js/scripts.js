05023077ebe426d08a6620bda87690cf9b2a2105
kuj1 <the.same.dentist@gmail.com>
2022-01-26 06:00:11 +0300

    fixed and refactored layout / code, added onlclick events

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,3 +1,35 @@
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
+var all_cont = document.querySelectorAll('.card_contacts');
+
+[].forEach.call( all_cont, function(el) {
+
+    el.onclick = function(e) {
+        console.log(el);
+        var text = el.innerText;
+        document.getElementById('srch').value = text;
+        console.log(text);
+    }
+});
+
+var all_author = document.querySelectorAll('.card_author');
+
+[].forEach.call( all_author, function(el) {
+
+    el.onclick = function(e) {
+        console.log(el);
+        var text = el.innerText;
+        document.getElementById('srch').value = text;
+        console.log(text);
+    }
+});
+
+var all_exs = document.querySelectorAll('.exs');
+
+[].forEach.call( all_exs, function(el) {
+
+    el.onclick = function(e) {
+        console.log(el);
+        var text = el.innerText;
+        document.getElementById('srch').value = text;
+        console.log(text);
+    }
+});
